829afcc3cb647dfbc92131a8943c646b1f5ea65c
girish <girishchukka@gmail.com>
2018-03-21 23:24:51 +0530

    Fix to exit readThread; Added mutex init & destroy

--- twoThreads.c
+++ twoThreads.c
@@ -6,6 +6,8 @@
 
 volatile char a;
 volatile int exitRead = 0, printVal = 1, readVal = 0;
+pthread_mutex_t mutexLock;
+
 void* readThread(void* vargp)
 {
   while(1)
@@ -14,6 +16,8 @@ void* readThread(void* vargp)
     scanf("%c", &a);
     readVal = 1;
     printVal = 0;
+    if(a=='e')
+      break;
   }
   exitRead = 1;
 }
@@ -33,11 +37,15 @@ int main()
 {
   pthread_t readT, writeT;
 
+  pthread_mutex_init(&mutexLock, NULL);
+
   pthread_create(&readT, NULL, &readThread, NULL);
   pthread_create(&writeT, NULL, &writeThread, NULL);
 
   pthread_join(readT, NULL);
   pthread_join(writeT, NULL);
+
+  pthread_mutex_destroy(&mutexLock);
 
   return 0;
 }
